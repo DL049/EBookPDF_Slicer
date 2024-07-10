63d4daaf4f7a1d677f999f3a3a4a6aad70566bd3
DL <75907889+DL049@users.noreply.github.com>
2024-07-10 15:06:30 +0800

    Add files via upload
    add folder system based on input pdf name and generate slice data record file.

--- main.py
+++ main.py
@@ -2,15 +2,22 @@ import streamlit
 import pandas as pd
 import fitz
 import re
-
+import os
+def createTOCFile(bookName,toc_list):
+    TOCFILE = open(bookName+"\Slice_Data.txt","x")
+    TOCFILE.write(bookName+"\n")
+    for chapter in toc_list:
+        TOCFILE.write(str(chapter[0])+" | "+chapter[1]+" | "+str(chapter[2])+"\n")
+    TOCFILE.close()
 def validFilename(filename:str):
     validify = re.sub('\W+',"_",filename)
     return validify
 #end is not included
-def createSubPDF(reader,start,end,filename:str):
+def createSubPDF(reader,start,end,filename:str,bookName):
     newPDF = fitz.open()
     newPDF.insert_pdf(reader,start-1,end-2)
-    newPDF.save("output\\"+("_").join(filename.split(" "))+".pdf")
+    
+    newPDF.save(bookName+"\\"+("_").join(filename.split(" "))+".pdf")
 
 
 def simplifyToc(Toc):
@@ -42,9 +49,13 @@ if pdf_file != None:
     streamlit.write(f"There will be {len(toc_l1)-2} Sliced PDFs.")
 
     buttonSlice = streamlit.button("Slice")    
+    folderName = validFilename(pdf_file.name)
     if buttonSlice:
+        os.mkdir(folderName)
+        createTOCFile(folderName,toc_l1)
         for i in range(len(toc_l1)-1):
-            createSubPDF(Reader,toc_l1[i][2],toc_l1[i+1][2],validFilename(toc_l1[i][1]))
+            createSubPDF(Reader,toc_l1[i][2],toc_l1[i+1][2],validFilename(toc_l1[i][1]),folderName)
+        
         
 
 
